5242f714599b68be2d5529260834f004afb99cef
BingSoy <yummojammo49@gmail.com>
2022-01-26 09:44:13 +1100

    cat header links
    - temp

--- components/header_cat.js
+++ components/header_cat.js
@@ -7,12 +7,12 @@ class Div extends HTMLElement {
     this.innerHTML = `
  <div class="cnav2" id="myTopnav2">
   <a class="index" href="index.html">HOME</a>|
-  <a class="aaa" href="index.html">ART</a>
-  <a class="aaa" href="index.html">MUSIC</a>
-  <a class="aaa" href="index.html">ANIMATIONS</a>|
-  <a class="aaa" href="index.html">MINECRAFT</a>
-  <a class="aaa" href="index.html">MONSTER HUNTER</a>
-  <a class="aaa" href="index.html">TF2</a>|
+  <a class="aaa" href="https://www.deviantart.com/bingsoy" target="_blank">ART</a>
+  <a class="aaa" href="https://bingsoy.bandcamp.com/" target="_blank">MUSIC</a>
+  <a class="aaa" href="https://www.youtube.com/BingSoyAnimations" target="_blank">ANIMATIONS</a>|
+  <a class="aaa" href="https://www.minecraft.net" target="_blank">MINECRAFT</a>
+  <a class="aaa" href="https://www.youtube.com/results?search_query=monster+hunter" target="_blank">MONSTER HUNTER</a>
+  <a class="aaa" href="https://www.youtube.com/watch?v=WHvwijT2ss8" target="_blank">TF2</a>|
   <a class="aaa" href="https://www.youtube.com/watch?v=vqCOss4hqnE" target="_blank">???</a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
