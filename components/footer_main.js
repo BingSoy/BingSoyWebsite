81c4e6969f5ee45aecaecb6b15bf23cfe01c0897
BingSoy <yummojammo49@gmail.com>
2022-02-09 02:19:24 +1100

    credits link

--- components/footer_main.js
+++ components/footer_main.js
@@ -8,7 +8,7 @@ class Div2 extends HTMLElement {
  <div class="cnav2" id="myTopnav4">
   <a class="aaa" href="tributea">Tribute A</i></a>
   <a class="aaa" href="tributeb">Tribute B</i></a>
-  <a class="aaa" href="">Credits</i></a>
+  <a class="aaa" href="credits">Credits</i></a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()"></a>
  </div>
     `;
