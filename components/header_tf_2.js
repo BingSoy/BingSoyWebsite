820c87fc71068ceab826b6fd7bef8029c5cd6729
BingSoy <yummojammo49@gmail.com>
2022-04-01 08:26:32 +1100

    correction
    - thanks link fix
    - also last commit /tf2 -> /teamfortress

--- components/header_tf_2.js
+++ components/header_tf_2.js
@@ -8,7 +8,7 @@ class Div2 extends HTMLElement {
  <div class="cnav3" id="myTopnav4">
   <a class="fixgame" href="fixgame">Fix Game</i></a>
   <a class="subclass" href="subclass">Subclass</i></a>
-  <a class="thanks" href="thank-you">Thanks</i></a>
+  <a class="thanks" href="thanks">Thanks</i></a>
   <a class="tribute" href="tribute">Tribute</i></a>
   <a class="timeline" href="timeline">Timeline</i></a>
   <a class="" href="https://gamebanana.com/members/2048161" target="_blank">GameBanana</i>&nbsp;<i class="fas fa-external-link-alt"></i></a>
