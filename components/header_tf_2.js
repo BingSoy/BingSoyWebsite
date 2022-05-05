59a1dbae54da5a86c5364bd216c23f2eee9be5ab
BingSoy <yummojammo49@gmail.com>
2022-05-05 13:29:56 +1000

    scoring page prototype

--- components/header_tf_2.js
+++ components/header_tf_2.js
@@ -12,6 +12,7 @@ class Div2 extends HTMLElement {
   <a class="tribute" href="tribute">Tribute</i></a>
   <a class="timeline" href="timeline">Timeline</i></a>
   <a class="mods" href="mods">Mods</i></a>
+  <a class="scoring" href="scoring">Scoring</i></a>
   <a class="" href="https://gamebanana.com/members/2048161" target="_blank">GameBanana</i>&nbsp;<i class="fas fa-external-link-alt"></i></a>
   <a href="https://www.youtube.com/channel/UCXeVMuFXaP4juglhff4a3tQ" target="blank">Second Channel&nbsp;<i class="fas fa-external-link-alt"></i></a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
