a28d4e9480c5adb4237876f297d2af97fb642b49
BingSoy <yummojammo49@gmail.com>
2022-09-17 18:39:36 +1000

    random link button upgrade
    https://learninginhand.com/html-for-random-link/

--- components/header_cat.js
+++ components/header_cat.js
@@ -11,7 +11,7 @@ class Div extends HTMLElement {
   <a class="" href="https://bingsoy.bandcamp.com/" target="_blank">MUSIC&nbsp;<i class="fas fa-external-link-alt"></i></a>
   <a class="" href="https://www.youtube.com/BingSoyAnimations" target="_blank">ANIMATIONS&nbsp;<i class="fas fa-external-link-alt"></i></a>|
   <a class="c-tf2" href="https://bingsoy.com/teamfortress/">TF2</a>|
-  <a class="" href="https://www.youtube.com/watch?v=vqCOss4hqnE" target="_blank">???&nbsp;<i class="fas fa-external-link-alt"></i></a>
+  <a class="" href="https://bingsoy.com/threeconsecutivequestionmarks" target="_blank">???&nbsp;<i class="fas fa-external-link-alt"></i></a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
