554b3e6e3f7839906c65a7925b33a13d1c7f0964
BingSoy <yummojammo49@gmail.com>
2022-09-29 00:36:35 +1000

    strategy page external link steam guide migration
    - rogue <i> tag fixing in tf_2 header
    - easier to update and format steam guide for the strategy

--- components/header_tf_2.js
+++ components/header_tf_2.js
@@ -6,16 +6,25 @@ class Div2 extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
  <div class="cnav3" id="myTopnav4">
-  <a class="fixgame" href="fixgame">Fix Game</i></a>
-  <a class="strategy" href="strategy">Strategy</i></a>
-  <a class="thanks" href="thanks">Thanks</i></a>
-  <a class="tribute" href="tribute">Tribute</i></a>
-  <a class="timeline" href="timeline">Timeline</i></a>
-  <a class="mods" href="mods">Mods</i></a>
-  <a class="scoring" href="scoring">Scoring</i></a>
-  <a class="faq" href="faq">FAQ</i></a>
-  <a class="" href="https://gamebanana.com/members/2048161" target="_blank">GameBanana</i>&nbsp;<i class="fas fa-external-link-alt"></i></a>
-  <a href="https://www.youtube.com/channel/UCXeVMuFXaP4juglhff4a3tQ" target="blank">Second Channel&nbsp;<i class="fas fa-external-link-alt"></i></a>
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars"></i>
+  </a>
+  <a class="fixgame" href="fixgame">Fix Game</a>
+  <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=2868711419" target="blank">
+    Strategy&nbsp;<i class="fas fa-external-link-alt"></i></a>
+  <a class="thanks" href="thanks">Thanks</a>
+  <a class="tribute" href="tribute">Tribute</a>
+  <a class="timeline" href="timeline">Timeline</a>
+  <a class="mods" href="mods">Mods</a>
+  <a class="scoring" href="scoring">Scoring</a>
+  <a class="faq" href="faq">FAQ</a>
+  <a class="" href="https://gamebanana.com/members/2048161" target="_blank">
+    <i>GameBanana</i>&nbsp;<i class="fas fa-external-link-alt"></i>
+  </a>
+  <a href="https://www.youtube.com/channel/UCXeVMuFXaP4juglhff4a3tQ" target="blank">
+    Second Channel&nbsp;
+    <i class="fas fa-external-link-alt"></i>
+  </a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
