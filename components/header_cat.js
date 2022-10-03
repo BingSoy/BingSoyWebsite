8aae33bd0625538269ac6ee1cc8cdf84c5a49d4d
BingSoy <yummojammo49@gmail.com>
2022-10-04 07:39:44 +1100

    philosophy page pilot, favicons update

--- components/header_cat.js
+++ components/header_cat.js
@@ -9,8 +9,12 @@ class Div extends HTMLElement {
   <a class="c-main" href="https://bingsoy.com">MAIN</a>|
   <a class="" href="https://www.deviantart.com/bingsoy" target="_blank">ART&nbsp;<i class="fas fa-external-link-alt"></i></a>
   <a class="" href="https://bingsoy.bandcamp.com/" target="_blank">MUSIC&nbsp;<i class="fas fa-external-link-alt"></i></a>
-  <a class="" href="https://www.youtube.com/BingSoyAnimations" target="_blank">ANIMATIONS&nbsp;<i class="fas fa-external-link-alt"></i></a>|
-  <a class="c-tf2" href="https://bingsoy.com/teamfortress/">TF2</a>|
+  <a class="" href="https://www.youtube.com/BingSoyAnimations" target="_blank">ANIMATIONS&nbsp;<i class="fas fa-external-link-alt"></i></a>
+  |
+  <a class="c-tf2" href="https://bingsoy.com/teamfortress/">TF2</a>
+  |
+  <a class="c-phi" href="https://bingsoy.com/philosophy/">Φ</a>
+  |
   <a class="" href="https://bingsoy.com/threeconsecutivequestionmarks" target="_blank">???&nbsp;<i class="fas fa-external-link-alt"></i></a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
