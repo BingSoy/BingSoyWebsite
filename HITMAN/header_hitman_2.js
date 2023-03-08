9467f6874279839ee7abb5dd2768fe89312e7019
BingSoy <yummojammo49@gmail.com>
2023-03-08 23:02:21 +1100

    hitman glossary, about

--- HITMAN/header_hitman_2.js
+++ HITMAN/header_hitman_2.js
@@ -7,10 +7,10 @@ class Div2 extends HTMLElement {
     this.innerHTML = `
  <div class="cnav3" id="myTopnav4">
   
-  <!--<a href="javascript:void(0);" class="icon" onclick="myFunction()">
+  <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
-  <a class="faq" href="LINK HERE">gtgeulgtnhdt</a>-->
+  <a class="faq" href="glossary">Glossary</a>
 
   <a class="" href="https://youtu.be/0K2JYq58ZxM" target="_blank">
     <i>Always online discussion</i>&nbsp;<i class="fas fa-external-link-alt"></i>
