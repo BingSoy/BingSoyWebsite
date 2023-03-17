b35c5fe0842a10693aa250c6a2955bb18f604112
BingSoy <yummojammo49@gmail.com>
2023-03-18 09:25:39 +1100

    cat navbar center, overflow hidden fix

--- media/art/header_art_1.js
+++ media/art/header_art_1.js
@@ -7,7 +7,6 @@ class Header extends HTMLElement {
     this.innerHTML = `
  <div class="cnav" id="myTopnav">
   <a class="index" href="https://bingsoy.com/teamfortress/">Home</a>
-  <a class="about" href="about">About</a>
   <a href="https://www.deviantart.com/bingsoy" target="blank">Gallery (dA)&nbsp;<i class="fas fa-external-link-alt"></i></a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
