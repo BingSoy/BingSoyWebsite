30783eabd8d5dd70a981a8cc91b4731151298af9
BingSoy <yummojammo49@gmail.com>
2025-08-11 21:47:06 +1000

    main site: live & discord links

--- resComponents/header_main.js
+++ resComponents/header_main.js
@@ -10,6 +10,15 @@ class Header extends HTMLElement {
   <a class="socials" href="socials">Socials</a>
   <a class="about" href="about">About</a>
   <a class="projects" href="projects">Projects</a>
+  <a href="https://www.youtube.com/@BingSoyLIVE" target="blank">
+    🔴LIVE&nbsp;
+    <i class="fas fa-external-link-alt"></i>
+  </a>
+  <a href="https://discord.gg/5E2xguXnwX" target="blank">
+  <i class="fa-brands fa-discord"></i>
+    Discord&nbsp;
+    <i class="fas fa-external-link-alt"></i>
+  </a>
   <a href="javascript:void(0);" class="icon" onclick="myFunction()">
     <i class="fa fa-bars"></i>
   </a>
